ecd656a79aab1c39fb89325a2eeee9015bcfddd1
server-lan <walji@mail.com>
2025-02-14 08:54:17 +0000

    added tile menu

--- pages/form.py
+++ pages/form.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
 
 st.title("Habits Management")
+selected = option_menu(None, ["Home", "Form"], 
+    icons=['house', "list-task"], 
+    menu_icon="cast", default_index=1, orientation="vertical")
+if selected == "Home":
+    st.switch_page("streamlit_app.py")
 st.write(
     "Use the below form to add new habits to the database:"
 )
@@ -16,3 +23,4 @@ with st.form("Add a Habit", True):
         st.write("Habit Name -", habit_name)
         st.write("Repeat (Y/N) -", habit_repeat)
         st.write("Notes -", habit_notes)
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,37 @@
 import streamlit as st
 import sqlalchemy as db
 import pandas as pd
+from streamlit_option_menu import option_menu
 
-st.set_page_config(page_title="Home")
+st.set_page_config(page_title="Home",initial_sidebar_state="collapsed")
 
 
 
 st.title("Home")
+selected = option_menu(None, ["Home", "Form"], 
+    icons=['house', "list-task"], 
+    menu_icon="cast", default_index=0, orientation="vertical")
+if selected == "Form":
+    st.switch_page("pages/form.py")
 st.write(
     "Welcome to the Habit Tracker! Please select a tile to get started."
 )
 
-def menu():
-    st.sidebar.page_link("pages/form.py", label="TEST")
-
-
-menu()
 
 
 
 
-'''engine = db.create_engine("sqlite:///habits.db")
-query = 'select * from habits'
 
-conn = engine.connect()
 
-df = pd.read_sql(
-    sql=query,
-    con=conn.connection
-)
 
-st.write(df)'''
+#engine = db.create_engine("sqlite:///habits.db")
+#query = 'select * from habits'
+
+#conn = engine.connect()
+
+#df = pd.read_sql(
+#    sql=query,
+ #   con=conn.connection
+#)
+
+#st.write(df)
